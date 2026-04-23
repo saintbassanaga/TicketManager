d2255ba0cccb2d299d67b92e0648a844c6e96830
Ngah Angele <ngaherica56@gmail.com>
2026-04-23 21:31:12 +0200

    Commit

--- TicketManager/settings.py
+++ TicketManager/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'TicketManager.urls'
+ROOT_URLCONF = 'ticketmanager.urls'
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'TicketManager.wsgi.application'
+WSGI_APPLICATION = 'ticketmanager.wsgi.application'
 
 
 # Database
--- ticketmanager/serializers.py
+++ ticketmanager/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Event, Ticket
+from .models import Event, Ticket, Order, OrderItem
 
 class TicketSerializer(serializers.ModelSerializer):
     class Meta:
@@ -13,3 +13,16 @@ class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = ['id', 'name', 'description', 'date', 'location', 'is_active', 'tickets']
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = ['id', 'order', 'ticket', 'quantity', 'price_at_purchase']
+
+class OrderSerializer(serializers.ModelSerializer):
+    # Permet de voir les items associés lors de la consultation d'une commande
+    items = OrderItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Order
+        fields = ['id', 'user', 'order_date', 'total_amount', 'is_paid', 'items']
